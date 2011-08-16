16611488ac961376004557d7ba647098df1ee2fa
ivanp <ivan@primaguna.com>
2011-08-16 19:11:19 +0700

    Rename admin to index

--- protected/admin/views/header/admin.php
+++ protected/admin/views/header/admin.php
@@ -44,7 +44,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'columns'=>array(
 		'id',
 		'name',
-		'created',
+		'created:datetime',
 		array(
 			'class'=>'CButtonColumn',
 		),
